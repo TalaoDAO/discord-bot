fa6b616970f8305a9d137a44457e3ce547592da7
achille1017 <achillemarseille@gmail.com>
2023-03-23 10:58:59 +0100

    clean code

--- bots/verifier.py
+++ bots/verifier.py
@@ -10,19 +10,21 @@ from datetime import datetime
 from discord import app_commands
 import sqlite3 as sql
 import logging 
+#logging.basicConfig(level = logging.INFO)
+
+discord_token=json.dumps(json.load(open("../keys.json", "r"))['discord_token'])[1:-1]
+
 
 try:
     sql.connect("database.db").cursor().execute("CREATE TABLE IF NOT EXISTS links(guild_id INTEGER,message_id INTEGER,role TEXT,vc TEXT)")
     sql.connect("database.db").cursor().execute("CREATE TABLE IF NOT EXISTS data_collecting(guild_id INTEGER,message_id INTEGER,channel TEXT,vc TEXT)")
-
 except:
     logging.warning("error DB")
 
+
 s = requests.Session()
+urlStream = 'https://talao.co/discord-bot/verifier_stream'
 
-    
-
-urlStream = 'https://0271-86-229-94-232.eu.ngrok.io/bot-verifier/verifier_stream'
 
 def verifying_data(role,id,payload,c):
    with s.get(urlStream, headers=None, stream=True) as resp:
@@ -34,15 +36,13 @@ def verifying_data(role,id,payload,c):
                         if data["id"]==id:
                             if data["check"]=="ok" :
                                 client.loop.create_task(payload.member.send("Thank you for verifying your "+data["typeCredential"]+'. You can now use your new permissions.'))   
-                                #await message.author.send(data["typeCredential"]+' Verified')                                 
                                 roleObj = discord.utils.get(client.get_guild(payload.guild_id).roles, name=role)
-                                #await message.author.add_roles(role)
                                 client.loop.create_task(payload.member.add_roles(roleObj))
                                 return
                             else:
                                 client.loop.create_task(payload.member.send('Error'))   
-                                #await message.author.send('Error')
                                 return
+
 
 def collecting_data(channel,id,payload,c):
    with s.get(urlStream, headers=None, stream=True) as resp:
@@ -55,31 +55,19 @@ def collecting_data(channel,id,payload,c):
                             if data["check"]=="ok" :
                                 client.loop.create_task(payload.member.send("Thank you for presenting your "+data["typeCredential"]+'.'))   
                                 presentation=data["presentation"]
-                                print(presentation)
-                                print(type(presentation))
-                                #.decode('utf8').replace("'", '"')
                                 dataP = json.loads(presentation)
-                                print(dataP)
-                                print(type(dataP))
-                                
-                                #["verifiableCredential"]["credentialSubject"]
-                                #await message.author.send(data["typeCredential"]+' Verified')                                 
-                                #roleObj = discord.utils.get(client.get_guild(payload.guild_id).roles, name=role)
-                                #await message.author.add_roles(role)
-                                print(payload)
                                 client.loop.create_task(discord.utils.get(client.get_guild(payload.guild_id).channels,name=channel).send(payload.member.name+" presented "+str(dataP["verifiableCredential"]["credentialSubject"])))
                                 return
                             else:
                                 client.loop.create_task(payload.member.send('Error'))   
-                                #await message.author.send('Error')
                                 return
+
 
 class MyClient(discord.Client):
     def __init__(self):
         super().__init__(intents=discord.Intents.default())
         self.synced=False
     async def send_message(self,member,message):
-        print(message)
         await member.send(message)
 
     async def on_ready(self):
@@ -87,8 +75,8 @@ class MyClient(discord.Client):
         if not self.synced:
             await tree.sync()
             self.synced=True
-            print("synced")
-        print('Logged on as', self.user)
+            logging.info("synced")
+        logging.info('Logged on as', self.user)
     
     async def on_message(self, message):
         # don't respond to ourselves
@@ -116,15 +104,8 @@ class MyClient(discord.Client):
                 logging.error('SQLite error: %s', ' '.join(er.args))
             if len(select)==0:
                 return
-
-
-            
-        #urlToQuery=""
-        #card=""
-        urlToQuery='https://0271-86-229-94-232.eu.ngrok.io/bot-verifier/init/'+select[0][0]
+        urlToQuery='https://talao.co/discord-bot/init/'+select[0][0]
         card=select[0][0]
-
-
         response = requests.get(urlToQuery)
         url=response.json()["url"]
         id=response.json()["id"]
@@ -142,11 +123,11 @@ class MyClient(discord.Client):
             thread.start()
         
 
-
 intents = discord.Intents.default()
 intents.message_content = True
 client = MyClient()
 tree=app_commands.CommandTree(client)
+
 
 @tree.command(name="add_verification",description="Add a verifial credential verification to give roles.")
 async def self(interaction: discord.Interaction, role:str,vc:str,channel:str):
@@ -156,14 +137,12 @@ async def self(interaction: discord.Interaction, role:str,vc:str,channel:str):
     roleObj = discord.utils.get(interaction.guild.roles, name=role)
     msg = await discord.utils.get(interaction.guild.channels,name=channel).send("React to this message to verify your "+vc+" and get the role "+role+".")
     message_idDB = msg.id
-
     try:
         with sql.connect("database.db") as con:
             cur = con.cursor()
             cur.execute("INSERT INTO links (guild_id,message_id,role,vc) VALUES (?,?,?,?)",(interaction.guild_id,message_idDB,role,vc) )
             con.commit()
             msg = "Link "+vc+" to "+role+" successfully added."
-
     except:
         con.rollback()
         msg = "error in insert operation" 
@@ -172,23 +151,18 @@ async def self(interaction: discord.Interaction, role:str,vc:str,channel:str):
         logging.info("msg db %s", msg)
         await interaction.response.send_message(msg)
 
+
 @tree.command(name="add_collection",description="Add a verifial credential collection to give roles.")
 async def self(interaction: discord.Interaction,vc:str,channel_bot:str,channel_reception:str):
-    #await interaction.response.send_message(f"Hello {name}")
     guild_id=interaction.guild_id
-    #channel = discord.utils.get(interaction.guild.channels,name="over-13").id
-    #roleObj = discord.utils.get(interaction.guild.roles, name=role)
     msg = await discord.utils.get(interaction.guild.channels,name=channel_bot).send("React to this message to present us your "+vc+".")
     message_idDB = msg.id
-
     try:
         with sql.connect("database.db") as con:
             cur = con.cursor()
             cur.execute("INSERT INTO data_collecting (guild_id,message_id,channel,vc) VALUES (?,?,?,?)",(interaction.guild_id,message_idDB,channel_reception,vc) )
             con.commit()
             msg = "Data collection for "+vc+" ready."
-
-
     except:
         con.rollback()
         msg = "error in insert operation" 
@@ -197,5 +171,5 @@ async def self(interaction: discord.Interaction,vc:str,channel_bot:str,channel_r
         logging.info("msg db %s", msg)
         await interaction.response.send_message(msg)
 
-client.run('MTA4MjY2NDMxODA0ODQxMTczOA.GOMXWx.bn43J7sU_wrRBeOS_YVBps1Wee-rZ2iwny4rA4')
+client.run(str(discord_token))
 
--- main.py
+++ main.py
@@ -7,21 +7,19 @@ import random
 import os
 import environment
 
-app = Flask(__name__,static_folder=os.path.abspath('/home/achille/altme-identity/static'))
-app.secret_key ="""json.dumps(json.load(open("keys.json", "r"))["appSecretKey"])"""
 
-characters = string.digits
+app = Flask(__name__)
+app.secret_key json.dumps(json.load(open("keys.json", "r"))["appSecretKey"])
 
-#init environnement variable
+
 myenv = os.getenv('MYENV')
 if not myenv :
    myenv='thierry'
 
 mode = environment.currentMode(myenv)
-
 red= redis.Redis(host='127.0.0.1', port=6379, db=0)
-
 did_verifier = 'did:tz:tz2NQkPq3FFA3zGAyG8kLcWatGbeXpHMu7yk'
+
 pattern={"type": "VerifiablePresentationRequest",
             "query": [
                 {
@@ -29,6 +27,7 @@ pattern={"type": "VerifiablePresentationRequest",
                     "credentialQuery": []
                 }]
             }
+
 patternSpec = {"type": "VerifiablePresentationRequest",
             "query": [
                 {
@@ -41,6 +40,7 @@ patternSpec = {"type": "VerifiablePresentationRequest",
                 }]
                 }]
             }
+
 patternNationality={"type": "VerifiablePresentationRequest",
             "query": [
                 {
@@ -53,7 +53,9 @@ patternNationality={"type": "VerifiablePresentationRequest",
                 }]
                 }]
             }
-@app.route('/bot-verifier/init/<typeP>')
+
+
+@app.route('/discord-bot/init/<typeP>')
 def verifier_init(typeP):
     id = str(uuid.uuid1())
     if typeP=="fr" or typeP=="en":
@@ -62,65 +64,46 @@ def verifier_init(typeP):
         patternToSend=patternSpec
         patternToSend["query"][0]["credentialQuery"][0]["example"]["type"]=typeP
     patternToSend['challenge'] = str(uuid.uuid1()) # nonce
-    """IP=extract_ip()
-    patternToSend['domain'] = 'http://' + IP"""
-    patternToSend['domain']="https://0271-86-229-94-232.eu.ngrok.io"
-    # l'idee ici est de créer un endpoint dynamique
-    red.set(id,  json.dumps(patternToSend))
-    url = 'https://0271-86-229-94-232.eu.ngrok.io/bot-verifier/endpoint/' + id +'?issuer=' + did_verifier
+    patternToSend['domain']=mode.server
+    red.setex(id,  json.dumps(patternToSend))
+    url = mode.server+'discord-bot/endpoint/' + id +'?issuer=' + did_verifier
     return jsonify({"url":url,"id":id}),200
 
 
-@app.route('/bot-verifier/endpoint/<id>', methods = ['GET', 'POST'],  defaults={'red' : red})
+@app.route('/discord-bot/endpoint/<id>', methods = ['GET', 'POST'],  defaults={'red' : red})
 def presentation_endpoint(id, red):
     try :
         my_pattern = json.loads(red.get(id).decode())
-        #pprint("my_pattern "+str(my_pattern))
     except :
         event_data = json.dumps({"id" : id,
                                  "message" : "redis decode failed",
                                  "check" : "ko"})
         red.publish('verifier', event_data)
-        #pprint("event data "+str(event_data))
         return jsonify("server error"), 500
     
     if request.method == 'GET':
-        #pprint("my_pattern "+str(my_pattern))
         return jsonify(my_pattern)
     
     if request.method == 'POST' :
         #red.delete(id)
         try : 
-            print(request.form['presentation'])
-            #result = json.loads(asyncio.run(verifyPresentation(request.form['presentation'])))
             result=False
-            print("result "+str(result))
         except:
-            print("except")
             event_data = json.dumps({"id" : id,
                                     "check" : "ko",
                                     "message" : "presentation is not correct"})
             red.publish('verifier', event_data)
             return jsonify("presentation is not correct"), 403
         if result :
-            print("result")
             event_data = json.dumps({"id" : id,
                                     "check" : "ko",
                                     "message" : result})
             red.publish('verifier', event_data)
             return jsonify(result), 403
-        # mettre les tests pour verifier la cohérence entre issuer, holder et credentialSubject.id 
-        # 
-        red.set(id,  request.form['presentation'])
+        red.setex(id,  request.form['presentation'])
         presentation=request.form['presentation']
-        print(type(presentation))
-        #credential = json.dumps(presentation, indent=4, ensure_ascii=False)
-        #presentation = json.dumps(presentation, indent=4, ensure_ascii=False)
         dictionnaire=json.loads(presentation)
-        #print(presentation)
-        print(type(dictionnaire))
         typeCredential=dictionnaire["verifiableCredential"]["type"][1]
-        print("type credential : "+typeCredential)
         event_data = json.dumps({"id" : id,
                                 "message" : "presentation is verified",
                                 "check" : "ok","typeCredential":typeCredential,
@@ -129,8 +112,8 @@ def presentation_endpoint(id, red):
         
         return jsonify("ok"), 200
 
-# server event push, peut etre remplacé par websocket
-@app.route('/bot-verifier/verifier_stream', methods = ['GET'],  defaults={'red' : red})
+
+@app.route('/discord-bot/verifier_stream', methods = ['GET'],  defaults={'red' : red})
 def presentation_stream(red):
     def event_stream(red):
         pubsub = red.pubsub()
@@ -142,5 +125,6 @@ def presentation_stream(red):
                 "Cache-Control" : "no-cache",
                 "X-Accel-Buffering" : "no"}
     return Response(event_stream(red), headers=headers)
+
 
 app.run(host = "localhost", port= 3000, debug=True)
